5f3759ff602b25375c63822e1d5dbd9dc4cf7706
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-19 13:02:06 +0100

    feat: Add Jsonc class for JSONC file handling
    This commit introduces a new class, Jsonc, which is designed to handle
    JSONC files. The Jsonc class has similar functionality to the existing
    Json class, but it also supports comments in JSON, which is a feature
    of JSONC. This class uses the jsonc-parser library for parsing and
    modifying JSONC data. In addition, unit tests for the Jsonc class have
    been added to ensure its functionality.

--- mod_test.ts
+++ mod_test.ts
@@ -1,11 +1,12 @@
 import { test } from "@cross/test";
-import { assertEquals } from "@std/assert";
+import { assertEquals, assertThrows } from "@std/assert";
 
 import { tmpdir } from "node:os";
 import { join } from "node:path";
-import { mkdirSync, readFileSync } from "node:fs";
+import { mkdirSync, readFileSync, writeFileSync } from "node:fs";
+import * as jsonc_parser from "jsonc-parser";
 
-import { Json, Text } from "./mod.ts";
+import { Json, Jsonc, Text } from "./mod.ts";
 
 function prepareTmpdir() {
   const randomStr = Math.random().toString(36).substring(7);
@@ -54,4 +55,87 @@ test("Json with validator", () => {
   const data = readFileSync(filepath, "utf-8");
   const json = JSON.parse(data);
   assertEquals(json, { hello: "world" });
+});
+
+test("Jsonc without validator", () => {
+  const td = prepareTmpdir();
+  const filepath = join(td, "file.jsonc");
+  {
+    using json = new Jsonc(filepath);
+    json.data = { hello: "world" };
+  }
+
+  const data = readFileSync(filepath, "utf-8");
+  const jsonc = jsonc_parser.parse(data);
+
+  assertEquals(jsonc, { hello: "world" });
+});
+
+test("Jsonc with prepared file and without validator", () => {
+  const td = prepareTmpdir();
+  const filepath = join(td, "file.jsonc");
+  const dummyJsonc = '{"foo": "bar", } // comment';
+  writeFileSync(filepath, dummyJsonc);
+
+  {
+    using json = new Jsonc(filepath);
+
+    assertEquals(json.data, { foo: "bar" });
+  }
+});
+
+test("Jsonc with validator", () => {
+  const td = prepareTmpdir();
+  const filepath = join(td, "file.jsonc");
+
+  function validator(data: unknown): data is { hello: string } {
+    return typeof data === "object" && data != null && "hello" in data;
+  }
+  {
+    using json = new Jsonc(filepath, validator);
+    json.data = { hello: "world" };
+  }
+
+  const data = readFileSync(filepath, "utf-8");
+  const jsonc = jsonc_parser.parse(data);
+  assertEquals(jsonc, { hello: "world" });
+});
+
+test("Jsonc with invalid data and validator", () => {
+  const td = prepareTmpdir();
+  const filepath = join(td, "file.jsonc");
+
+  function validator(data: unknown): data is { hello: string } {
+    return typeof data === "object" && data != null && "hello" in data;
+  }
+
+  writeFileSync(filepath, '{"foo": "bar"}');
+
+  assertThrows(() => {
+    using json = new Jsonc(filepath, validator);
+    assertEquals(json.data, { hello: "world" });
+  });
+});
+
+test("Jsonc with prepared file and valid data and validator", () => {
+  const td = prepareTmpdir();
+  const filepath = join(td, "file.jsonc");
+  const dummyJsonc = '{"hello": "world"} // comment';
+
+  writeFileSync(filepath, dummyJsonc);
+
+  function validator(data: unknown): data is { hello?: string; foo?: string } {
+    return typeof data === "object" && data != null && "hello" in data;
+  }
+
+  {
+    using json = new Jsonc(filepath, validator);
+    assertEquals(json.data, { hello: "world" });
+    json.data = { foo: "bar" };
+  }
+
+  const data = readFileSync(filepath, "utf-8");
+
+  const jsonc = jsonc_parser.parse(data);
+  assertEquals(jsonc, { foo: "bar" });
 });
