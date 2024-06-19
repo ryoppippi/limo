b713646972cdbecc42fa534d2edeeb06ea75daed
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-19 10:55:31 +0100

    feat: Add Text class for file handling
    This commit introduces a new class, Text, which implements the Limo
    interface. The Text class provides methods for reading and writing text
    files. It uses the dispose pattern to automatically write changes to the
    file when the instance is disposed. The commit also includes unit tests
    for the new functionality.

--- mod.ts
+++ mod.ts
@@ -1,3 +1,58 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
+
+interface Limo<T> {
+  [Symbol.dispose](): void;
+
+  get data(): T | undefined;
+  set data(value: T);
+}
+
+/**
+ * Text
+ *
+ * Read and automatically write text files.
+ * @example
+ * ```ts
+ * import { Text } from "@ryoppippi/limo";
+ * {
+ *   using text = new Text("file.txt");
+ *   text.data = "Hello, World!";
+ * }
+ * ```
+ */
+export class Text implements Limo<string> {
+  #data: string | undefined;
+  #path: string;
+
+  constructor(path: string) {
+    this.#path = path;
+    this.#data = this._read();
+  }
+
+  [Symbol.dispose]() {
+    this._write();
+  }
+
+  get data(): string | undefined {
+    return this.#data;
+  }
+
+  set data(value: string) {
+    this.#data = value;
+  }
+
+  private _read() {
+    if (existsSync(this.#path)) {
+      return readFileSync(this.#path, { encoding: "utf8" });
+    }
+    return undefined;
+  }
+
+  private _write() {
+    if (this.#data == null) {
+      return;
+    }
+
+    writeFileSync(this.#path, this.#data);
+  }
 }
--- mod_test.ts
+++ mod_test.ts
@@ -1,8 +1,27 @@
 import { test } from "@cross/test";
 import { assertEquals } from "@std/assert";
 
-import { add } from "./mod.ts";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import { mkdirSync, readFileSync } from "node:fs";
 
-test("dummy", () => {
-  assertEquals(add(1, 2), 3);
+import { Text } from "./mod.ts";
+
+function prepareTmpdir() {
+  const randomStr = Math.random().toString(36).substring(7);
+  const td = join(tmpdir(), randomStr);
+  mkdirSync(td, { recursive: true });
+  return td;
+}
+
+test("Text", () => {
+  const td = prepareTmpdir();
+  const filepath = join(td, "file.txt");
+  {
+    using text = new Text(filepath);
+    text.data = "Hello, World!";
+  }
+
+  const data = readFileSync(filepath, "utf-8");
+  assertEquals(data, "Hello, World!");
 });
